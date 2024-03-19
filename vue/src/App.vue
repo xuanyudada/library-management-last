b531828fbfcb2a4553a535ee1d247eb727d43dac
xuanyu <3426464304@qq.com>
2024-03-19 22:26:22 +0800

    修改-02

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 头部区域   -->
+    <div style="height: 60px; line-height: 60px;background-color: white;margin-bottom: 2px">
+      <img src="@/assets/logo.png" alt="" style="width: 40px;position: relative;top:10px;left: 20px">
+      <span style="margin-left: 25px; font-size: 24px">图书管理系统</span>
+    </div>
+    <!--侧边栏和主体-->
+    <div style="display: flex">
+      <!-- 侧边栏导航-->
+      <div
+          style="width: 200px;min-height: calc(100vh - 62px); overflow: hidden;margin-right: 2px;background-color: white">
+        <el-menu :default-active="$route.path" router class="el-menu-demo">
+          <el-menu-item index="/">首页</el-menu-item>
+          <el-submenu>
+            <template slot="title">关于页面</template>
+            <el-menu-item index="/about">关于详情</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+
+      <!--主体数据-->
+      <div style="flex: 1;background-color: white">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
-<style>
-
-</style>
